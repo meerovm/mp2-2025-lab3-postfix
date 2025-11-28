21cd370dda542210d5d8563018a7f4588df98884
meerovm <meerov.m@gmail.com>
2025-11-28 16:05:40 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,83 @@
-// объявление функций и классов для вычисления арифметических выражений
+// РѕР±СЉСЏРІР»РµРЅРёРµ С„СѓРЅРєС†РёР№ Рё РєР»Р°СЃСЃРѕРІ РґР»СЏ РІС‹С‡РёСЃР»РµРЅРёСЏ Р°СЂРёС„РјРµС‚РёС‡РµСЃРєРёС… РІС‹СЂР°Р¶РµРЅРёР№
+#include "stack.h"
+#include <string>
+struct token {
+	int pr; 
+	int flag;
+	double p;
+	token() {
+		pr = -1;
+		p = 0;
+	}
+	token(std::string q, double pp = -1) {
+		if (q == "-") {
+			flag = 1;
+			pr = 1;
+		}
+		if (q == "+"){
+			flag = 2;
+			pr = 1;
+		}
+		if (q == "*") {
+			flag = 3;
+			pr = 2;
+		}
+		if (q == "/") {
+			flag = 4;
+			pr = 2;
+		}
+		if (q == "sin") {
+			flag = 44;
+			pr = 3;
+		}
+		if (q == "cos") {
+			flag = 5;
+			pr = 3;
+		}
+		if (q == "log") {
+			flag = 6;
+			pr = 3;
+		}
+		if (q == "exp") {
+			flag = 7;
+			pr = 3;
+		}
+		if (q == "number") {
+			flag = 8;
+			pr = 0;
+		}
+		if (q == "x") {
+			flag = 9;
+			pr = 0;
+		}
+		if (q == "y") {
+			flag = 10;
+			pr = 0;
+		}
+		if (q == "z"){
+			flag = 11;
+			pr = 0;
+		}
+		if (q == "(") {
+			flag = 12;
+			pr = 6;
+		}
+		p = pp;
+	}
+};
+class arithmetic {
+private:
+	Stack<token> q;
+public:
+	double x, y, z;
+	int flag_x, flag_y, flag_z;
+	arithmetic(std::string str);
+	arithmetic() {
+		q = Stack<token>();
+		x = 0;
+		y = 0;
+		z = 0;
+		flag_x = flag_y = flag_z = 0;
+	}
+	double calc();
+};
