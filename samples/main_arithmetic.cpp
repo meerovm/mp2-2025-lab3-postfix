258b389332e6b4c8a0311245a53ff89a8891c6dc
meerovm <meerov.m@gmail.com>
2025-11-28 16:06:23 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,31 @@
-// реализация пользовательского приложения
-
+#include<iostream>
+#include"arithmetic.h"
 int main()
 {
-  return 0;
+	std::string str = "";
+	int f = 0;
+	std::cout << " enter the expression or \" stop \" ";
+	std::cin >> str;
+	arithmetic c = arithmetic(str);
+	while (f == 0) {
+		if (c.flag_x == 1) {
+			std::cout << "enter x ";
+			std::cin >> c.x;
+			f = 1;
+		}
+		if (c.flag_y == 1) {
+			std::cout << "enter y ";
+			std::cin >> c.y;
+			f = 1;
+		}
+		if (c.flag_z == 1) {
+			std::cout << "enter z ";
+			std::cin >> c.z;
+			f = 1;
+		}
+		std::cout << c.calc() << std::endl;
+		std::cout << "enter 0 to calc with other variables ";
+		std::cin >> f;
+	}
+	return 0;
 }
